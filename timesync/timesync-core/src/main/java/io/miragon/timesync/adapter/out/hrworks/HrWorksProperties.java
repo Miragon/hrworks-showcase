128fdda60a5125b082f0ee35339e9a1607682bf9
Rami Nakkar <rami.nakkar@miragon.io>
2023-07-26 20:29:10 +0200

    feat: add properties validation

--- timesync/timesync-core/src/main/java/io/miragon/timesync/adapter/out/hrworks/HrWorksProperties.java
+++ timesync/timesync-core/src/main/java/io/miragon/timesync/adapter/out/hrworks/HrWorksProperties.java
@@ -1,5 +1,7 @@
 package io.miragon.timesync.adapter.out.hrworks;
 
+import jakarta.validation.Valid;
+import jakarta.validation.constraints.NotNull;
 import lombok.Getter;
 import lombok.Setter;
 import org.springframework.boot.context.properties.ConfigurationProperties;
@@ -7,10 +9,13 @@ import org.springframework.boot.context.properties.ConfigurationProperties;
 @ConfigurationProperties(prefix = "hrworks")
 @Setter
 @Getter
+@Valid
 public class HrWorksProperties {
 
+    @NotNull
     private String accessKey;
 
+    @NotNull
     private String secretAccessKey;
 
     private String baseUrl = "https://api-demo.hrworks.de/v2";
